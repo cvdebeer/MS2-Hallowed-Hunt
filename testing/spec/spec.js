adc21ae7b37de5b218b0c75bd92638d407790c65
cvdebeer <claudiadiebeer@gmail.com>
2019-09-26 19:51:54 +0100

    manage files and folders for testing

---
+++ testing/spec/spec.js
@@ -0,0 +1,14 @@
+describe("Purpose", function () {
+    var a;
+
+    it("Expectation", function () {
+        //arrange
+
+        a = true;
+        //act
+
+
+        //assert
+        expect(a).toBe(true);
+    });
+});
