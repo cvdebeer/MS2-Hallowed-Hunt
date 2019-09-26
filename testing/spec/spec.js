84d9f5ae9b927dbfc3208571cb7b5299262daf2b
cvdebeer <claudiadiebeer@gmail.com>
2019-09-26 20:45:03 +0100

    failing flipCard test

--- testing/spec/spec.js
+++ testing/spec/spec.js
@@ -12,3 +12,15 @@ describe("Purpose", function () {
         expect(a).toBe(true);
     });
 });
+
+describe("flipCard function has been defined", function () {
+    it("should exist", function () {
+        expect(flipCard).toBeDefined();
+    });
+    it(`should add the class 'flip' to the card`, function () {
+
+
+        flipCard();
+        expect(this.classList.toggle('.flip')).toBe(true);
+    })
+});
