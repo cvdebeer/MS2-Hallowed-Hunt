8b62d585da616125d4c23745ed03ae5df234a659
cvdebeer <claudiadiebeer@gmail.com>
2019-09-27 17:41:31 +0100

    testing the data-set

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,28 @@
 const cards = document.querySelectorAll('.game-card');
 
+let cardHasFlipped = false;
+let cardOne, cardTwo;
 
+// make the card flip
 function flipCard() {
     this.classList.toggle('flip');
+
+    //Picking two cards to match
+    if (!cardHasFlipped) {
+        cardHasFlipped = true;
+        cardOne = this;
+        // console.log({cardHasFlipped, cardOne});
+    } else {
+        cardHasFlipped = false;
+        cardTwo = this;
+        //console.log({cardOne,cardTwo});
+
+        // do cards match ??
+        console.log(cardOne.dataset.check);
+        console.log(cardTwo.dataset.check);
+    }
 }
+
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
