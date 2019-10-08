48298c4464fa5215486e95e4392191a1858e17ac
cvdebeer <claudiadiebeer@gmail.com>
2019-10-08 12:54:04 +0100

    remove test files. No Jasmine testing.

--- testing/scripts/file.js
+++
@@ -1,7 +0,0 @@
-const cards = document.querySelectorAll('.game-card');
-
-function flipCard() {
-    this.classList.toggle('.flip');
-}
-
-cards.forEach(card => card.addEventListener('click', flipCard));
